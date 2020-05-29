8d71fc08e7c9a24872bbcbae9bd13b24a89856cd
Ke Huang <keh@codeaurora.org>
2020-05-29 16:01:36 +0800

    meta-qti-wlan: Add cnss2 service and script
    Add cnss2 service and cnss dlkm load/unload scripts.
    
    Change-Id: I671e009dbbe0a0f34b29b8f9ffd041eba12f7634
    CRs-Fixed: 2697573

---
+++ recipes/wlan/files/init.qti.cnss2_off.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+# Copyright (c) 2020, The Linux Foundation. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above
+#       copyright notice, this list of conditions and the following
+#       disclaimer in the documentation and/or other materials provided
+#       with the distribution.
+#     * Neither the name of The Linux Foundation nor the names of its
+#       contributors may be used to endorse or promote products derived
+#       from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+# IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+#
+
+echo -n "start to unlaod cnss2 module" > /dev/kmsg
+if (lspci -k|grep cnss_pci);then
+	echo -n "start to unlaod wlan driver before unload cnss2" > /dev/kmsg
+	if (lspci -k|grep 1102);then
+		echo -n "unload qca6595" > /dev/kmsg
+		modprobe -r qca6595
+	elif ((lspci -k|grep 003e) || (lspci -k|grep QCA6174));then
+		echo -n "unload qca6574" > /dev/kmsg
+		modprobe -r qca6574
+	elif (lspci -k|grep 1101);then
+		echo -n "unload qca6696" > /dev/kmsg
+		modprobe -r qca6696
+	else
+		echo -n "unload default wlan" > /dev/kmsg
+		modprobe -r wlan
+	fi
+	echo -n "unload cnss2" > /dev/kmsg
+	modprobe -r cnss2
+fi
+echo -n "unload wlanhost driver done" > /dev/kmsg
+
