4ba4ceee4b13c88620ad10de19d634bd01aa6f10
Wu Gao <quic_wugao@quicinc.com>
2022-09-26 23:54:25 -0700

    meta-qti-wlan: Add wlan modules for sdx75
    This change adds wlan modules for sdx75.
    
    Change-Id: I9ba5836e33f0ee00e370850bfbef5eed6d770733

--- qtiwlan.mk
+++ qtiwlan.mk
@@ -1,6 +1,5 @@
-QTIWLAN:= qcacld32-ll wlan-conf wpa-supplicant-8-lib hostapd-daemon wlan-cnss wlan-sigma-dut wpa-supplicant-qcacld
+QTIWLAN:= wlan-conf wpa-supplicant-8-lib hostapd-daemon wlan-sigma-dut wpa-supplicant-qcacld
 
-## Eg: Add target specific packages.
-##ifeq ($(TARGET_MACHINE),sdx75)
-##  QTIWLAN+=newloc-package
-##endif
+ifeq ($(TARGET_MACHINE),sdx75)
+  QTIWLAN+=kmod-qcacld32-ll kmod-wlan-cnss
+endif
