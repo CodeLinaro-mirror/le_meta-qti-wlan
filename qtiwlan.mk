363de5b6e638c5596d7e700c6a253d7479c3ba26
Xinyue Ling <xinyling@qti.qualcomm.com>
2026-04-03 14:30:40 +0800

    meta-qti-wlan: Enable emesh-sp-mcc for SDX85 mbb-min
    CRs-Fixed: 4475884
    Change-Id: I26d08d84a86c7106f80dd2c12f1c582ef08169d1

--- qtiwlan.mk
+++ qtiwlan.mk
@@ -1,12 +1,20 @@
-QTIWLAN:= wlan-conf wpa-supplicant-8-lib hostapd-daemon wlan-sigma-dut  wpa-supplicant-qcacld wlan-uci
+include $(INCLUDE_DIR)/target.mk
+
+ifeq ($(PROFILE),mbb-min)
+  QTIWLAN:=
+else
+  QTIWLAN:= wlan-conf wpa-supplicant-8-lib hostapd-daemon wlan-sigma-dut  wpa-supplicant-qcacld wlan-uci
+endif
 
 ifeq ($(BOARD),echo)
   QTIWLAN+=kmod-qcacld32-ll kmod-wlan-cnss2 cld80211-lib kmod-wlan-dts-oss
 endif
 
 ifeq ($(BOARD),sdx85)
-  QTIWLAN+=kmod-qcacld32-ll kmod-wlan-cnss2 cld80211-lib
   QTIWLAN+=kmod-emesh-sp-mcc
+ifneq ($(PROFILE),mbb-min)
+  QTIWLAN+=kmod-qcacld32-ll kmod-wlan-cnss2 cld80211-lib
+endif
 endif
 
 ifeq ($(BOARD),sdx75)
