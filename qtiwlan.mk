5c1ba41e44c53d1e75b93f6a903182b27730d5b9
Xinyue Ling <xinyling@qti.qualcomm.com>
2026-04-07 14:55:37 +0800

    meta-qti-wlan: Define QTIWLANMIN for mbb-min
    mbb-min profile requires enablement of emesh-sp-mcc.
    
    CRs-Fixed: 4475884
    Change-Id: Ia61fb6fc3d841d9595747e5fb61aaed00f129fc8

--- qtiwlan.mk
+++ qtiwlan.mk
@@ -1,20 +1,13 @@
-include $(INCLUDE_DIR)/target.mk
-
-ifeq ($(PROFILE),mbb-min)
-  QTIWLAN:=
-else
-  QTIWLAN:= wlan-conf wpa-supplicant-8-lib hostapd-daemon wlan-sigma-dut  wpa-supplicant-qcacld wlan-uci
-endif
+QTIWLAN:= wlan-conf wpa-supplicant-8-lib hostapd-daemon wlan-sigma-dut  wpa-supplicant-qcacld wlan-uci
+QTIWLANMIN:=kmod-emesh-sp-mcc
 
 ifeq ($(BOARD),echo)
   QTIWLAN+=kmod-qcacld32-ll kmod-wlan-cnss2 cld80211-lib kmod-wlan-dts-oss
 endif
 
 ifeq ($(BOARD),sdx85)
-  QTIWLAN+=kmod-emesh-sp-mcc
-ifneq ($(PROFILE),mbb-min)
   QTIWLAN+=kmod-qcacld32-ll kmod-wlan-cnss2 cld80211-lib
-endif
+  QTIWLAN+=kmod-emesh-sp-mcc
 endif
 
 ifeq ($(BOARD),sdx75)
